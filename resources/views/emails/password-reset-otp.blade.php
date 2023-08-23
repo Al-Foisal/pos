839d783a3ab7c36847fb8b225bf1c6f161f533af
Md Hafiz Al Foisal <alfoisal940700@gmail.com>
2023-08-23 13:04:16 +0600

    mail system updated

--- resources/views/emails/password-reset-otp.blade.php
+++ resources/views/emails/password-reset-otp.blade.php
@@ -1,19 +1,10 @@
 <x-mail::message>
-@slot('header')
-@component('mail::header', ['url' => config('app.url')])
-<!-- header here -->
-<img src="{{ asset($company->logo) }}" class="logo" alt="Logo">
-@endcomponent
-@endslot
+
 # Password Rest Otp
 
 Your password reset OTP is {{ $otp }}
 
 Thanks,<br>
 {{ $company->name }}
-@slot('footer')
-@component('mail::footer')
-© {{ date("Y") }} {{ $company->name }}. All rights reserved.
-@endcomponent
-@endslot
+
 </x-mail::message>
